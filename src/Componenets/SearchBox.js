2563ceffdeb5eeb0f623c43c638655977416a508
claire-me <charlesclaire101@gmail.com>
2021-07-16 11:00:18 +0100

    initial commit

--- src/Componenets/SearchBox.js
+++ src/Componenets/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBox = (props) => {
     return (
         <input 
-            className='tc pa2'
+            className='tc pa2 search-input'
             type='search'
             placeholder={props.placeholder}
         
